53fcc89d115888fdaf53e6b29bdba429699f331f
klhhhhh <1412841649@qq.com>
2025-02-11 00:54:24 -0800

    fix scripts for ds

--- scripts/DeepSpeed/multinode.sh
+++ scripts/DeepSpeed/multinode.sh
@@ -1,4 +1,5 @@
-deepspeed --num_nodes=4 --num_gpus=4 \
-    --master_addr="nid200421" \
-    --master_port=29500 \
-    /global/homes/k/klhhhhh/ckpt-fault-tolerance-test/data_parallel/saving/multi_saving/llama-3.2-1B/multi_gpu_tensor_core.py
+srun deepspeed --num_nodes=4 --num_gpus=4 \
+    --master_addr="nid200292" \
+    --master_port=6510 \
+    --hostfile="/global/homes/k/klhhhhh/ckpt-fault-tolerance-test/scripts/hostfile" \
+    /global/homes/k/klhhhhh/ckpt-fault-tolerance-test/data_parallel/saving/multi_saving/llama-3.2-1B/multi_gpu_tensor_core.py &> output.log
