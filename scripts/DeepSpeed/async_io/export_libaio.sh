ba810c869e6d62062de9ac3476270e14c8588bdc
klhhhhh <1412841649@qq.com>
2025-02-22 17:37:22 -0800

    add import script for aysnc_io libaio

---
+++ scripts/DeepSpeed/async_io/export_libaio.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$HOME/local/include:$C_INCLUDE_PATH
+export CFLAGS="-I/global/homes/k/klhhhhh/local/include $CFLAGS"
+export LDFLAGS="-L/global/homes/k/klhhhhh/local/lib $LDFLAGS"
