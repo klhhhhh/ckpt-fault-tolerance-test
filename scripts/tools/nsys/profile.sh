3fc748d82e38827b546354ca6bf7033fee53ab03
klhhhhh <1412841649@qq.com>
2025-03-09 20:08:01 -0700

    update nsys script

--- scripts/tools/nsys/profile.sh
+++ scripts/tools/nsys/profile.sh
@@ -1,5 +1,17 @@
-nsys profile -o async_fp16_profile.txt deepspeed --num_nodes=4 --num_gpus=4 \
-    --master_addr="nid200476" \
+##ALL gpu processes are profiled
+nsys profile -o async_fp16_profile \
+    --trace=cuda,nvtx,osrt \
+    deepspeed --num_nodes=4 --num_gpus=4 \
+    --master_addr="nid200357" \
     --master_port=29501 \
     --hostfile="/global/homes/k/klhhhhh/ckpt-fault-tolerance-test/scripts/hostfile" \
     /global/homes/k/klhhhhh/ckpt-fault-tolerance-test/data_parallel/saving/multi_saving/llama-3.2-1B/async_io/train_test.py "FP16" "async" &> nsys_async_io_async_FP16_output.log
+
+##only for rank 0 gpu 0
+CUDA_VISIBLE_DEVICES=0 RANK=0 nsys --capture-range=cudaProfilerApi --output=async_fp16_profile \
+    --trace=cuda,nvtx,osrt \
+    deepspeed --num_nodes=4 --num_gpus=4 \
+    --master_addr="nid200357" \
+    --master_port=29501 \
+    --hostfile="/global/homes/k/klhhhhh/ckpt-fault-tolerance-test/scripts/hostfile" \
+    /global/homes/k/klhhhhh/ckpt-fault-tolerance-test/data_parallel/saving/multi_saving/llama-3.2-1B/async_io/train_test_profile.py "FP16" "async" &> nsys_async_io_async_FP16_output.log
